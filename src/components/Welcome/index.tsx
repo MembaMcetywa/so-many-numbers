63ecc77edd2f6e3e691c6f911c266ce29795b24a
Membathisi Mcetywa <iamemba10gmail.com>
2023-09-17 22:56:06 +0200

    feature(components): added welcome component + styles

---
+++ src/components/Welcome/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Welcome.css";
+
+const Welcome = () => {
+  return <header>Welcome to So many numbers, I lost count at 122</header>;
+};
+
+export default Welcome;
